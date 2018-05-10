2f7c3c8f5d7051a3772d0ee8634830c229129c17
Natan Oliveira <natannjos@gmail.com>
2018-05-10 20:37:18 -0300

    Muda algumas coisas

--- bikeapp/settings.py
+++ bikeapp/settings.py
@@ -19,14 +19,12 @@ PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.0/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'd6528aqd21zk%&3v+*+lzjj&to!l$r%kbti99cj6cr&vf_*3d3'
 
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
 
-ALLOWED_HOSTS = ['104.131.69.57', 'testenv.com.br', 'www.testenv.com.br']
+# SECURITY WARNING: don't run with debug turned on in production!
+
+ALLOWED_HOSTS = ['167.99.9.19', 'testenv.com.br', 'www.testenv.com.br']
 
 
 # Application definition
