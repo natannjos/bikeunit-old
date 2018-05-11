bd353fd81d1e20366cd6d62a340c369f5be13a06
Natan Oliveira <natannjos@gmail.com>
2018-05-10 21:54:04 -0300

    Adiciona arquivos estaticos no core

--- bikeapp/settings.py
+++ bikeapp/settings.py
@@ -121,13 +121,13 @@ AUTH_PASSWORD_VALIDATORS = [
 ]
 
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')
 STATIC_URL = '/static/'
 
 # Extra places for collectstatic to find static files.
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-)
+# STATICFILES_DIRS = (
+#     os.path.join(BASE_DIR, 'static'),
+# )
 
 LOGIN_URL = 'login'
 LOGOUT_URL = 'logout'
