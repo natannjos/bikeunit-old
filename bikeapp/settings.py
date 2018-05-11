8c6db515a1f3732e407dbd18cd6197031c724015
Natan Oliveira <natannjos@gmail.com>
2018-05-10 21:30:36 -0300

    Adiciona static

--- bikeapp/settings.py
+++ bikeapp/settings.py
@@ -120,8 +120,8 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-STATIC_URL = 'bikeapp/static/'
-STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 LOGIN_URL = 'login'
 LOGOUT_URL = 'logout'
