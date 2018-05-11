3e798db94bc113c730b31a0af2ee0d70cd36aca1
Natan Oliveira <natannjos@gmail.com>
2018-05-11 13:18:48 -0300

    Altera staticroot

--- bikeapp/settings.py
+++ bikeapp/settings.py
@@ -122,7 +122,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # Extra places for collectstatic to find static files.
 # STATICFILES_DIRS = (
