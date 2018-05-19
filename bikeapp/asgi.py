91c1c05ab511a9fbeccda0fa770782813e6167c1
Natan Oliveira <natannjos@gmail.com>
2018-05-19 11:49:14 -0300

    Altera asgi

--- bikeapp/asgi.py
+++ bikeapp/asgi.py
@@ -1,1 +1,12 @@
-ASGI_APPLICATION = "bikeapp.routing.application"
+"""
+ASGI entrypoint. Configures Django and then runs the application
+defined in the ASGI_APPLICATION setting.
+"""
+
+import os
+import django
+from channels.routing import get_default_application
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "bikeapp.settings")
+django.setup()
+application = get_default_application()
