8017d2d6bf1c894ffdca06341e62c592624f95c7
Natan Oliveira <natannjos@gmail.com>
2018-05-29 11:25:03 -0300

    Minor changes on views

--- chat/views.py
+++ chat/views.py
@@ -4,20 +4,25 @@ from pusher import Pusher
 from django.views.decorators.csrf import csrf_exempt
 from django.shortcuts import HttpResponse
 
+try:
+    import bikeapp.local_settings
+    ssl = False
+except ImportError:
+    ssl= True
+
 pusher = Pusher(
     app_id='533241',
     key='9e1445ca72ab1228b6c6',
     secret='fa4c120420de83455d40',
     cluster='us2',
-    ssl=True
+    ssl=ssl
 )
 
 class Chat(LoginRequiredMixin, TemplateView):
     login_url = '/admin/login/'
     template_name = 'chat/chat.html'
 
-    
 @csrf_exempt
-def broadcast(request):
+def broadcast(request, *args, **kwargs):
     pusher.trigger('my_channel', 'my_event', {'name': request.user.username, 'message': request.POST['message']})
     return HttpResponse('done')
